45a86cfefb53f9bb123edc845d7b817993bc556e
ShylkoVad <shylkovad@gmail.com>
2023-02-13 22:25:34 +0300

    13.02.2023 - 22:25

---
+++ src/by/teachmeskils/homeworks/hw_17022023/Loop.java
@@ -0,0 +1,15 @@
+package by.teachmeskils.homeworks.hw_17022023;
+
+public class Loop {
+    public static void main(String[] args) {
+        int line = 4;
+        int column = 5;
+
+        for (int y = 0; y < line; ++y) {
+            for (int x = 0; x < column; ++x) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ src/by/teachmeskils/homeworks/hw_17022023/Switch.java
@@ -0,0 +1,33 @@
+package by.teachmeskils.homeworks.hw_17022023;
+
+public class Switch {
+    public static void main(String[] args) {
+        int n = 8;
+
+        switch (n){
+            case 1, 8:
+                System.out.println("Сегодня понедельник");
+                break;
+            case 2, 9:
+                System.out.println("Сегодня вторник");
+                break;
+            case 3:
+                System.out.println("Сегодня среда");
+                break;
+            case 4:
+                System.out.println("Сегодня четверг");
+                break;
+            case 5:
+                System.out.println("Сегодня пятница");
+                break;
+            case 6:
+                System.out.println("Сегодня суббота");
+                break;
+            case 7:
+                System.out.println("Сегодня воскресенье");
+                break;
+            default:
+                System.out.println("Не корректно введено число недели");
+        }
+    }
+}
