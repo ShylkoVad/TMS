6341c5def214a1f65a4be5a3e5d398fdcd86cc65
ShylkoVad <shylkovad@gmail.com>
2023-03-05 15:24:23 +0300

    05/03/2023 - 15:24

--- src/by/teachmeskils/homeworks/hw_10032023/enumeration/enumAlphabet/Main.java
+++
@@ -1,7 +0,0 @@
-package by.teachmeskils.homeworks.hw_10032023.enumeration.enumAlphabet;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(EnumAlphabet.X.getLetterPosition());
-    }
-}
--- src/by/teachmeskils/homeworks/hw_10032023/string/Main.java
+++
@@ -1,9 +0,0 @@
-package by.teachmeskils.homeworks.hw_10032023.string;
-
-public class Main {
-
-    public static void main(String[] args) {
-        StringUtils.returnCharacter();
-        StringUtils.checkEndString();
-    }
-}
